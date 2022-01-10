540a97e21a7992b4b0f04b58f9a3dcda0686d330
gkdlvjdhrkaw <gkdlvjdhrkaw@gmail.com>
2022-01-10 12:52:05 +0900

    [22.01.10] # 자바 초기 커밋

---
+++ src/main/java/com/spring/core/oop/hotel/Chef.java
@@ -0,0 +1,6 @@
+package com.spring.core.oop.hotel;
+
+public interface Chef {
+
+    void cook();
+}
---
+++ src/test/java/com/spring/core/oop/hotel/HotelTest.java
@@ -0,0 +1,19 @@
+package com.spring.core.oop.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HotelTest {
+
+    @Test
+    void reserveTest() {
+
+        //Hotel hotel = new Hotel(new KoreanRestaurant(new KimChef()));
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(HotelConfig.class);
+        Hotel hotel = context.getBean(Hotel.class);
+        hotel.reserve();
+    }
+
+}
